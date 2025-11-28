ec12863ce53a86bfc546f96755dbaa8550ea19cd
LarissaScarlat <alarissascarlat@gmail.com>
2025-11-28 16:08:03 -0300

    Commit

---
+++ PedidosDiaria.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import fs from 'fs';
+import axios from 'axios';
+import 'dotenv/config';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+function readTokens() {
+ try {
+   const data = fs.readFileSync('tokens.json', 'utf-8');
+   return JSON.parse(data);
+ } catch (error) {
+   console.error("Erro ao ler tokens.json:", error);
+   return null;
+ }
+}
+
+const router = express.Router();
+
+
+router.get ("/pedidospreparação", async (req, res) => {
+try {
+    const { dataInicial, dataFinal } = req.query;
+
+if (!dataInicial || !dataFinal) {
+    return res.status(400).json({ error: "Parâmetros dataInicial e dataFinal são obrigatórios." });
+}
+}
+}
+)
